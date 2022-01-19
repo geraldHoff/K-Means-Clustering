d87468e222b5059dc85127ff4126073f1e0ec9f9
GeraldH0ff <gerald.hoff@wsu.edu>
2022-01-19 12:53:17 -0800

    now populates withdots

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,11 @@ import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import java.util.Random;
 
 public class Main extends Application {
 
@@ -28,19 +30,30 @@ public class Main extends Application {
         // create a scene
         Scene scene = new Scene(root, 400, 400);
 
+        Random rand = new Random();
+
+        int d = 10000;
+        for(int i = 0; i < d; i++){
+            Circle circle = new Circle(
+                    rand.nextInt(1600), rand.nextInt(800), rand.nextInt(10),
+                    Color.rgb(rand.nextInt(255), rand.nextInt(255), rand.nextInt(255))
+            );
+            root.getChildren().add(circle);
+        }
+
         scene.setOnMousePressed(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent event) {
-                Circle circle = new Circle(event.getX(), event.getY(), 5);
-                System.out.println(event.getX() + " " + event.getY());
+                Circle circle = new Circle(event.getX(), event.getY(), 5,
+                        Color.rgb(rand.nextInt(255), rand.nextInt(255), rand.nextInt(255))
+                );
                 root.getChildren().add(circle);
             }
         });
 
         // set the scene
-        window.setScene(scene);
-
         window.setMaximized(true);
+        window.setScene(scene);
         window.show();
     }
 
